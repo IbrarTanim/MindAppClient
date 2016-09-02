a0b7ddb75d923a34b0dbd7a73cbbbcda7b161ca5
IbrarTanim <ibrar.tanim@gmail.com>
2016-09-02 08:27:49 +0600

    First commit

---
+++ app/src/main/java/com/rokan/mindappclient/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rokan.mindappclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+           //Rokan first Commit
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
